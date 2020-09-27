eb95d13a699d31bcf8ba8433df167ccc6a018838
Oliver-DA <71343538+Oliver-DA@users.noreply.github.com>
2020-09-27 14:34:38 -0400

    updated script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ document.addEventListener('DOMContentLoaded',() => {
     const getById = id => document.getElementById(id);
 
     const userNameInput = getById('name');
+    const emailInput = getById("mail");
     const title = getById("title");
     const jobRole = getById('other-title');
     const design = getById("design");
     const colors = getById("color");
+    const paymentInfo = getById("payment")
+    const paypal = getById("paypal");
+    const bitCoin = getById("bitcoin");
+    const creditCard = getById("credit-card")
 
     //Starting the form
     jobRole.hidden = true;
@@ -50,8 +55,97 @@ document.addEventListener('DOMContentLoaded',() => {
         }
     })
 
+    //Activities
+    const actControl = document.querySelector(".activities");
+    const activities = document.querySelectorAll(".activities input");
+    let totalCost = 0;
+    const costElement = document.createElement("span");
+
+    actControl.insertAdjacentElement("beforeend",costElement);
 
 
+    actControl.addEventListener('change', e => {
+        const target = e.target;
+        const attr = "data-day-and-time";
+
+        target.checked
+        ? totalCost += Number(target.getAttribute("data-cost"))
+        : totalCost -= Number(target.getAttribute("data-cost"))
+
+        costElement.textContent = `Total: $${totalCost}`;
+        
+        for (let a of activities) {
+
+            if (a.getAttribute(attr) === target.getAttribute(attr) && target !== a){
+
+                (a.disabled = target.checked ? true:false )
+            }
+        }
+    });
+
+
+    //Payment Information
+
+    const paymentMethods = [paypal,bitCoin,creditCard];
+
+    function showPayment(methods,select){
+        
+        for (let method of methods){
+            method.hidden = method.id === select ? false:true
+        }
+    }
+
+    showPayment(paymentMethods,"credit-card")
+
+    paymentInfo[1].selected = true;
+    paymentInfo[0].disabled = true;
+
+    paymentInfo.addEventListener('change', e => {
+        const target = e.target.value;
+        showPayment(paymentMethods,target)
+    });
+
+    //Validations
+
+
+    function isValidName () {
+        const name = userNameInput.value;
+
+        if (name.length > 0) {
+            userNameInput.style.border = "2px solid rgb(111, 157, 220)";
+            return true
+        }
+        userNameInput.style.border = "2px solid red";
+        return false
+    }
+
+    userNameInput.addEventListener("blur",e => {
+        isValidName();
+    })
+
+    function isValidEmail(){
+        const email = emailInput.value;
+        const ad = email.indexOf("@");
+        const dot = email.lastIndexOf(".");
+
+        if (ad > 0 && dot > ad + 1){
+            emailInput.style.border = "2px solid rgb(111, 157, 220)";
+            return true
+        }
+        emailInput.style.border = "2px solid red";
+        return false
+    }
+    emailInput.addEventListener("blur",e =>{
+        isValidEmail()
+    })
+
+    function isValidActtivitie () {
+
+    }
+
+    function isValidCreditCard () {
+
+    }
 
 
 
